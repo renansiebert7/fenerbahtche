2c83f337cf59e966143e06de0834a23b0dc6c3d7
renansiebert7 <renansiebert01@gmail.com>
2025-08-25 20:40:53 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1>FENERBAHTCHÊ</h1>
+<br>
+<br>
+<h2>ESSE É O MEU PROJETO DO TIME DE FUTSAL FENERBAHTCHÊ</h2>
+<br>
+<br>
+<h4> 
+Projeto criado para representar o time de futebol Fenerbahtche, fundado em 2023 por um grupo de amigos da escola, no qual sou um dos fundadores.
+
+O site apresenta:
+  <ul> 
+    <li>Jogadores e comissão técnica</li>
+    <li>Camisa oficial da torcida</li>
+    <li>Patrocinadores do time</li>
+  </ul>
+<br>
+Projeto autoral com foco em organização de conteúdo, identidade visual e estruturação de página estática. </h4> 
+<br>
+<h3>Tecnologias Utilizadas:</h3>
+<ul>
+  <li> HTML </li>
+  <li> CSS </li>
+  <li> JAVA SCRIPT </li>
+</ul>
+<br>
+<br>
+Acesse o Projeto Aqui:
+<br>
+https://renansiebert7.github.io/fenerbahtche-project/
+<br>
+<br>
+<img src="https://raw.githubusercontent.com/renansiebert7/fenerbahtche-project/refs/heads/master/assets/FENERBAHTCH%C3%8A.png">
---
+++ compra das camisas/java.js
@@ -0,0 +1,34 @@
+const form = document.querySelector(".form-camisas")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function cliqueiNoBotao() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function fecharFormulario() {
+    form.style.left = "-350px"
+    form.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden";
+}
+
+document.querySelector('.form-camisas').addEventListener('submit', function (event) {
+    event.preventDefault(); // Impede o envio padrão
+
+    const form = this;
+    const data = new FormData(form);
+
+    // Envia os dados do formulário para o Formcarry
+    fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: { 'Accept': 'application/json' }
+    }).then(() => {
+        // Redireciona para a página de pagamento
+        window.location.href = "https://renansiebert7.github.io/FENERBAS.3/";
+    }).catch(() => {
+        alert("Erro ao enviar o formulário. Tente novamente.");
+    });
+});

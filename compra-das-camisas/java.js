ab10606eff83361e713bc3b8311dbe58fd72c222
renansiebert7 <renansiebert01@gmail.com>
2025-08-25 23:06:58 -0300

    aa

--- compra-das-camisas/java.js
+++ compra-das-camisas/java.js
@@ -27,7 +27,7 @@ document.querySelector('.form-camisas').addEventListener('submit', function (eve
         headers: { 'Accept': 'application/json' }
     }).then(() => {
         // Redireciona para a página de pagamento
-        window.location.href = "pagamento/";
+        window.location.href = "/fenerbahtche/pagamento/";
     }).catch(() => {
         alert("Erro ao enviar o formulário. Tente novamente.");
     });
